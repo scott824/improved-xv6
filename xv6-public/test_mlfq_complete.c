e62b6717fcbf5168fbd961bba51dbaaf2cdab7df
Sangchul, Lee <scott824@naver.com>
2017-04-09 23:50:19 +0900

    MLFQ scheduler implement 1. add MLFQ logic in scheduler() 2. add test_mlfq_complete test program

---
+++ xv6-public/test_mlfq_complete.c
@@ -0,0 +1,42 @@
+/**
+ *  this program test only MLFQ concurrency
+ */
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define NAME_CHILD_MLFQ     "test_mlfq"
+
+char *child_argv[3][3] = {
+    {NAME_CHILD_MLFQ, "0", 0},
+    {NAME_CHILD_MLFQ, "1", 0},
+    {NAME_CHILD_MLFQ, "0", 0}
+};
+
+int
+main()
+{
+    int pid;
+    int i;
+
+    for(i = 0; i < 3; i++) {
+        pid = fork();
+        if (pid > 0) {
+            continue;
+        } else if (pid == 0) {
+            exec(child_argv[i][0], child_argv[i]);
+            printf(1, "exec failded!\n");
+            exit();
+        } else {
+            printf(1, "fork failed!\n");
+            exit();
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        wait();
+    }
+
+    exit();
+}
